ff831225037e364cfd6e522403a28dcc899433ae
maxgurewitz <maxthegeek1@gmail.com>
2018-07-01 10:54:04 -0700

    certbot endpoint, lodash

--- src/server/buildServer.ts
+++ src/server/buildServer.ts
@@ -1,4 +1,5 @@
 import * as Hapi from 'hapi';
+import _ from 'lodash';
 import Bluebird from 'bluebird';
 
 export default async function buildServer({ port } : { port?: number }): Bluebird<Hapi.Server> {
@@ -12,6 +13,14 @@ export default async function buildServer({ port } : { port?: number }): Bluebir
     path: '/',
     handler: (request, h) => {
         return 'Hello, world!';
+    }
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/.well-known/acme-challenge/{content}',
+    handler: (request, h) => {
+      return _.get(request, 'params.content', 'empty');
     }
   });
 
