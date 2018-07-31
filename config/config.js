85e2e8d9c050dcca4e67806bb9a3baa3eb430acb
maxgurewitz <maxthegeek1@gmail.com>
2018-07-30 23:17:12 -0700

    fix config syntax error

--- config/config.js
+++ config/config.js
@@ -20,7 +20,7 @@ module.exports = {
     // password: null,
     // database: 'client_manager_production',
     // host: process.env.DATABASE_URL.split(' ')[0],
-    use_env_variable: 'DATABASE_URL'
+    use_env_variable: 'DATABASE_URL',
     operatorsAliases: false,
     dialect: 'postgres'
   }
